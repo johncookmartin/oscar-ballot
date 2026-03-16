f5854797c0c65a7de4d6416872e74731e3aa8141
John Cook-Martin <johncookmartin@gmail.com>
2026-03-16 11:22:02 -0400

    enabling SSO

--- src/auth/msalToken.ts
+++ src/auth/msalToken.ts
@@ -1,6 +1,7 @@
-import type {
-  AccountInfo,
-  IPublicClientApplication,
+import {
+  InteractionRequiredAuthError,
+  type AccountInfo,
+  type IPublicClientApplication,
 } from '@azure/msal-browser';
 
 export async function acquiredApiAccessToken(params: {
@@ -10,16 +11,29 @@ export async function acquiredApiAccessToken(params: {
 }): Promise<string> {
   const { msal, account, scope } = params;
 
-  if (!account) {
-    throw new Error(
-      'No signed-in account. Call login first and setActiveAccount',
-    );
+  // if we have an account, try silent token acquisition first
+  if (account) {
+    try {
+      const result = await msal.acquireTokenSilent({
+        account,
+        scopes: [scope],
+      });
+      return result.accessToken;
+    } catch (error) {
+      if (!(error instanceof InteractionRequiredAuthError)) {
+        throw error;
+      }
+    }
   }
 
-  const result = await msal.acquireTokenSilent({
-    account,
-    scopes: [scope],
-  });
+  try {
+    const result = await msal.ssoSilent({
+      scopes: [scope],
+    });
 
-  return result.accessToken;
+    msal.setActiveAccount(result.account);
+    return result.accessToken;
+  } catch (_error) {
+    throw new Error('No active session. User must log in');
+  }
 }
