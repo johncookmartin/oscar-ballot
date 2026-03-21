1fcf6b064d6b7ab00afa65a73968bc13e33a1d45
John Cook-Martin <johncookmartin@gmail.com>
2026-03-21 19:13:19 -0400

    moving font import before msal initialization

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,15 @@ import { MsalProvider } from '@azure/msal-react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.ts';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/montserrat/300.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/700.css';
+
 const msalInstance = new PublicClientApplication(msalConfig);
 
 await msalInstance.initialize();
@@ -28,15 +37,6 @@ initApiAuth({
   getAccount: () =>
     msalInstance.getActiveAccount() ?? msalInstance.getAllAccounts()[0] ?? null,
 });
-
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '@fontsource/montserrat/300.css';
-import '@fontsource/montserrat/400.css';
-import '@fontsource/montserrat/500.css';
-import '@fontsource/montserrat/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
